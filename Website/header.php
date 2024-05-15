413c8f0e0eae0f54aa4dfb9b1a3731665a945e58
HIVE-LAP02\Chris <crecio@hivedatacenter.com>
2024-05-15 16:32:25 -0400

    Fix header but

--- Website/header.php
+++ Website/header.php
@@ -62,7 +62,11 @@
                             </li>
                         </ul>
                         <div class="" id="navText">
-                            Your IP: <?php echo $_SERVER['HTTP_CF_CONNECTING_IP']; ?>
+                            <?php
+                            if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+                                echo "Your IP:" . $_SERVER['HTTP_CF_CONNECTING_IP'];
+                            }
+                            ?>
                         </div>
                     </div>
                 </div>
